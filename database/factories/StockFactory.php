cba9c0e0eebe2f4f223e0d8defa6deea87a0fd86
DallingtonAsin <asingwire50dallington@gmail.com>
2023-03-09 16:35:12 +0300

    edited stock factory

--- database/factories/StockFactory.php
+++ database/factories/StockFactory.php
@@ -32,13 +32,13 @@ class StockFactory extends Factory
       'item_code' => Str::random(3),
       'item' => Str::random(7),
       'category_id' => $category_id,
-      'quantity' => $this->faker->randomDigit,
-      'threshold_qty' => $this->faker->randomDigit,
-      'buying_price' => $this->faker->numberBetween($min = 4000, $max = 6000),
-      'selling_price' => $this->faker->numberBetween($min = 6000, $max = 9000),
+      'quantity' => $this->faker->numberBetween(150, 300),
+      'threshold_qty' => $this->faker->numberBetween(5, 15),
+      'buying_price' => $this->faker->numberBetween(4000,6000),
+      'selling_price' => $this->faker->numberBetween(6000,9000),
       'supplier_id' => $supplier_id,
-      'date_of_entry' => $this->faker->dateTimeThisYear($max = 'now', $timezone = null),
-      'expiry_date' => $this->faker->dateTimeThisYear($max = 'now', $timezone = null),
+      'date_of_entry' => $this->faker->dateTimeThisYear('now', 'Africa/Kampala'),
+      'expiry_date' => $this->faker->dateTimeThisYear('now', 'Africa/Kampala'),
     ];
   }
 }
