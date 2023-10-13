ec92e46519ec9fb60ac3636e6f948bd20bc416f0
DallingtonAsin <asingwire50dallington@gmail.com>
2023-10-13 10:17:04 +0300

    updated import stock algorithm

--- app/Imports/ImportStock.php
+++ app/Imports/ImportStock.php
@@ -8,71 +8,62 @@ use App\Models\Stock;
 
 class ImportStock implements ToModel, WithHeadingRow
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
+  /**
+   * @param array $row
+   *
+   * @return \Illuminate\Database\Eloquent\Model|null
+   */
+  public function model(array $row)
+  {
 
-      //  dd($row);
-       $item_code = $row['barcode'];
-       $item = $row['item'];
-       $stock = $this->getStock();
+    $item_code = $row['barcode'];
+    $item = $row['item'];
 
-       (isset($item_code))
-       ? $item_code = $item_code
-       : $item_code = null;
+    (isset($item_code))
+      ? $item_code = $item_code
+      : $item_code = null;
 
-       if(in_array($item, $stock)){
-        
-        $qty = $this->getItemQty($item);
-        $newQty = $qty + floatval($row['quantity']);
-         Stock::where('item', $item)
-           ->update(['quantity' => $newQty]);
+    if (Stock::where('item_code', $item_code)->exists()) {
+      $qty = $this->getItemQty($item);
+      $newQty = $qty + floatval($row['quantity']);
+      Stock::where('item_code', $item_code)->update(['quantity' => $newQty]);
+    } else {
 
-       }
+      $whereClause  = [
+        'item_code' => $row['barcode']
+      ];
 
-       else
-       {
-         return new Stock([
-          'item_code' => $item_code,
-          'item' => $item,
-          'quantity' => floatval($row['quantity']),
-          'buying_price' => floatval($row['cost_price']),
-          'selling_price' => floatval($row['selling_price']),
-          'wholesale_price' => floatval($row['wholesale_price']),
-          'supplier' => $row['supplier']
-        ]);
+      return Stock::updateOrCreate($whereClause, [
+        'item_code' => $item_code,
+        'item' => $item,
+        'quantity' => floatval($row['quantity'] ? $row['quantity'] : 0),
+        'buying_price' => floatval($row['costprice'] ? $row['costprice'] : 0),
+        'selling_price' => floatval($row['sellingprice'] ? $row['sellingprice'] : 0),
+        'wholesale_price' => floatval($row['wholesaleprice'] ? $row['wholesaleprice'] : 0),
+        'supplier' => $row['supplier']
+      ]);
+    }
+  }
 
-       }
-       
+
+  protected function getStock()
+  {
+
+    $items = Stock::all();
+    $itemsArr = array();
+    foreach ($items as $item) {
+      array_push($itemsArr, $item->item);
     }
 
-
-    protected function getStock()
-    {
-
-      $items = Stock::all();
-      $itemsArr = array();
-      foreach ($items as $item) {
-        array_push($itemsArr, $item->item);
-      }
-
-      return $itemsArr;
+    return $itemsArr;
+  }
 
 
-    }
+  protected function getItemQty($item)
+  {
 
-
-    protected function getItemQty($item)
-    {
-      
-      $qty = Stock::where('item', $item)
-             ->value('quantity');
-      return $qty;
-    }
-
-
+    $qty = Stock::where('item', $item)
+      ->value('quantity');
+    return $qty;
+  }
 }
